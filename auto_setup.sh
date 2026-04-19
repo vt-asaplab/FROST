bc698a91697127c8d235aa71f2f710491e27133d
tunglethanh <90467128+tunglethanh@users.noreply.github.com>
2026-04-18 21:09:43 -0400

    Update auto_setup.sh

--- auto_setup.sh
+++ auto_setup.sh
@@ -44,11 +44,13 @@ echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$PREFIX/lib" >> /home/$USER/.bash
 
 # Install Python libraries
 sudo apt-get install python3-pip
+pip3 install numpy scipy matplotlib scikit-learn tqdm pytrends nltk
 
-# Note that if the setup process has the error: externally-managed-environment
+# Note that if the above setup process has the error: externally-managed-environment
 # Then, do this (replace 3.12 with the Python version in the system): 
 # sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/EXTERNALLY-MANAGED.old
-pip3 install numpy scipy matplotlib scikit-learn tqdm pytrends nltk
+# Followed by running again: 
+# pip3 install numpy scipy matplotlib scikit-learn tqdm pytrends nltk
 
 # Install libgsl for building graphm library
 sudo apt-get install libgsl-dev
