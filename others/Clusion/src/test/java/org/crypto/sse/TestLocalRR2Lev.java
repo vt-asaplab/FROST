acdd9f57c400e9101cdc73714b466b37889bf2d4
tunglethanh <90467128+tunglethanh@users.noreply.github.com>
2026-04-27 20:36:12 -0400

    Update TestLocalRR2Lev.java

--- others/Clusion/src/test/java/org/crypto/sse/TestLocalRR2Lev.java
+++ others/Clusion/src/test/java/org/crypto/sse/TestLocalRR2Lev.java
@@ -114,7 +114,7 @@ public class TestLocalRR2Lev {
 
 			System.out.println("Execution time: " + estimatedTime);
 
-			double bw = (result.size() * 4 * tpr + n * fpr + 16) / 1024.0;
+			double bw = (result.size() * sizeof(int) * tpr + n * fpr * sizeof(int) + 16) / 1024.0;
 			
 			System.out.println("Bandwidth cost: " + bw + " KB");
 		}
