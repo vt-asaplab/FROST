8fbf5fb948730364ad9e49be5e79d3d62ce36d50
tunglethanh <90467128+tunglethanh@users.noreply.github.com>
2026-04-29 17:35:06 -0400

    Update APOModules.java

--- others/APOSSE/src/main/java/org/apo/sse/APOModules.java
+++ others/APOSSE/src/main/java/org/apo/sse/APOModules.java
@@ -36,7 +36,42 @@ public class APOModules {
     private static double p = 0.895328521728516;
 	private static double q = 0.0210561086316476;
     public static final int BYTES_IN_INT = 4;
-
+	public static final Set<String> stopwords = Set.of("a", "about", "above", "across", "after",
+		"afterwards", "again", "against", "all", "almost", "alone", "along",
+		"already", "also", "although", "always", "am", "among", "amongst", "amoungst",
+		"amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway",
+		"anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become",
+		"becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below",
+		"beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by",
+		"call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de",
+		"describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight",
+		"either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever",
+		"every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify",
+		"fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found",
+		"four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt",
+		"have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon",
+		"hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if",
+		"in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last",
+		"latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile",
+		"might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must",
+		"my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine",
+		"no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off",
+		"often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise",
+		"our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please",
+		"put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious",
+		"several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so",
+		"some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere",
+		"still", "such", "system", "take", "ten", "than", "that", "the", "their", "them",
+		"themselves", "then", "thence", "there", "thereafter", "thereby", "therefore",
+		"therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those",
+		"though", "three", "through", "throughout", "thru", "thus", "to", "together", "too",
+		"top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up",
+		"upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when",
+		"whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein",
+		"whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever",
+		"whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet",
+		"you", "your", "yours", "yourself", "yourselves"); 
+	
 	public static void setParameters(int m, int k, double p, double q) {
 		APOModules.m = m;
 		APOModules.k = k;
@@ -81,7 +116,14 @@ public class APOModules {
 		
 		for (String keyword : listOfKeyword) {
 			count++;
+			
             System.out.printf("Progress: %.2f%% of keywords %n", (count * 100.0) / total);
+
+			if (stopwords.contains(keyword)) continue;
+
+            if (keyword.length() < 4 || keyword.length() > 20) continue;
+
+            if (!keyword.matches("[a-zA-Z]+")) continue;
 			
 			for (File file : listOfFile) {
 				if (keywordLists.get(keyword).contains(file.getName())) {
