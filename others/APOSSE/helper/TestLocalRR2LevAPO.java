868b2378314bff98611104a241d456dc9be8e357
tunglethanh <90467128+tunglethanh@users.noreply.github.com>
2026-04-29 15:56:46 -0400

    Update TestLocalRR2LevAPO.java

--- others/APOSSE/helper/TestLocalRR2LevAPO.java
+++ others/APOSSE/helper/TestLocalRR2LevAPO.java
@@ -104,7 +104,7 @@ public class TestLocalRR2LevAPO {
 		// The two parameters depend on the size of the dataset. Change
 		// accordingly to have better search performance
 		int bigBlock = 16384;
-		int smallBlock = 128;
+		int smallBlock = 1024;
 		int dataSize = 50000000;
 
 		// Construction of the global multi-map
