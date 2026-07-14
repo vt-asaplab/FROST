bde7e46408a89d7fb22438f9838c1c61dd9dd5c7
tunglethanh <90467128+tunglethanh@users.noreply.github.com>
2026-07-14 11:05:56 -0700

    Update auto_setup.sh

--- auto_setup.sh
+++ auto_setup.sh
@@ -21,7 +21,7 @@ fi
 # Install GMP
 export LDFLAGS="-L$PREFIX/lib/"
 export CPPFLAGS="-I$PREFIX/include/"
-wget https://gmplib.org/download/gmp/gmp-6.3.0.tar.xz
+wget https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz
 tar -xvf gmp-6.3.0.tar.xz
 cd gmp-6.3.0/
 ./configure --prefix="$PREFIX"
