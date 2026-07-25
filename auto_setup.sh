4edffd4e342f2dc74575543a11ebcb867c51d2ef
tunglethanh <90467128+tunglethanh@users.noreply.github.com>
2026-07-24 21:22:58 -0700

    Update auto_setup.sh

--- auto_setup.sh
+++ auto_setup.sh
@@ -43,8 +43,8 @@ make -j$(nproc)
 cd ../
 
 # Set library path
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib/
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$PREFIX/lib/" >> /home/$USER/.bashrc
+export LD_LIBRARY_PATH=$PREFIX/lib/:$LD_LIBRARY_PATH
+echo "export LD_LIBRARY_PATH=$PREFIX/lib/:\$LD_LIBRARY_PATH" >> /home/$USER/.bashrc
 source /home/$USER/.bashrc
 
 # Install Python libraries
